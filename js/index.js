48b5744479140f35665ae84cd38aa93755e35d6a
Yura <yurgayura44@gmail.com>
2021-10-23 13:33:51 +0300

    New task 3

--- js/index.js
+++ js/index.js
@@ -1,17 +1,36 @@
-function mOver(obj) {
-	obj.innerHTML = "Mouse on me!";
-}
+// function mOver(obj) {
+// 	obj.innerHTML = "Mouse on me!";
+// }
  
-function mOut(obj) {
-	obj.innerHTML = "Mouse isn't on me!";
-}
+// function mOut(obj) {
+// 	obj.innerHTML = "Mouse isn't on me!";
+// }
  
-function mDown(obj) {
-	obj.style.backgroundColor = "#1ec5e5";
-	obj.innerHTML = "I was pressed";
-}
+// function mDown(obj) {
+// 	obj.style.backgroundColor = "#1ec5e5";
+// 	obj.innerHTML = "I was pressed";
+// }
  
-function mUp(obj) {
-	obj.style.backgroundColor="#D94A38";
-	obj.innerHTML="Mouse on me!";
-}
+// function mUp(obj) {
+// 	obj.style.backgroundColor="#D94A38";
+// 	obj.innerHTML="Mouse on me!";
+// }
+
+
+const butt = document.getElementById('butt');
+
+butt.addEventListener("click", function(){
+	document.getElementById("window").innerText += 'I was pressed' + "\n";
+	console.log("I was pressed");
+});
+
+butt.addEventListener("mouseover", function(){
+	document.getElementById("window").innerText += 'Mouse on me'+ "\n";
+	console.log("Mouse on me");
+});
+
+butt.addEventListener("mouseout", function(){
+	document.getElementById("window").innerText += 'Mouse is not on me!'+ "\n";
+	console.log("Mouse on me");
+}); 
+
